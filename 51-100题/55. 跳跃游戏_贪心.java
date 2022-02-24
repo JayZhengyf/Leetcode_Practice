0f324e377a0d307d51fe3bc66de962621590b430
Jay Zheng <1219635918@qq.com>
2022-02-24 11:08:26 +0800

    第五十五题提交

---
+++ 51-100题/55. 跳跃游戏_贪心.java
@@ -0,0 +1,16 @@
+public class Solution {
+	public boolean canJump(int[] nums) {
+		int n = nums.length;
+		// 当前的边界
+		int rightmost = 0;
+		for (int i = 0; i < n; i++) {
+			if (i <= rightmost) {
+				rightmost = Math.max(rightmost, i + nums[i]);
+				if (rightmost >= n - 1) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
